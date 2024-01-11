836ecb5313f81df72cde8f8967b4581f70ac2cad
bpluim <ben.pluim@gmail.com>
2024-01-11 14:30:44 -0700

    pulled repo

--- obj/Debug/net8.0/LearnCSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/LearnCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01e86e43f4d4d65bcf5f369ef4f959ba6986b7d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
